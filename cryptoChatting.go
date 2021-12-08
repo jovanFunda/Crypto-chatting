4b242e49300a4b741e0e8b3a43a31dae3304302b
jovanFunda <jovanfunda18@gmail.com>
2021-12-08 21:04:28 +0100

    Testing github desktop

--- cryptoChatting.go
+++ cryptoChatting.go
@@ -82,7 +82,7 @@ func main() {
 	var sender *Node = newNode()
 	var receiver *Node = newNode()
 
-	var message, signature []byte = sender.SendMessage("Hello to you!", receiver.publicKey)
+	var message, signature []byte = sender.SendMessage("Hello world!", receiver.publicKey)
 
 	var receivedMessage []byte = receiver.ReadMessage(message)
 
